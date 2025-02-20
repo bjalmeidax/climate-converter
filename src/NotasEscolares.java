82dba7da6f0ba5640b1d63f4ced069ea685f554f
Gabriel J. Almeida <gjalmeida0309@gmail.com>
2025-02-20 13:15:50 -0300

    Exclusão de codigos teste

--- src/NotasEscolares.java
+++
@@ -1,9 +0,0 @@
-public class NotasEscolares {
-    public static void main(String[] args) {
-        double nota1 = 7;
-        double nota2 = 9.8;
-
-        double media = (nota1 + nota2) / 2;
-        System.out.println("A média do aluno foi de: " + media);
-    }
-}
